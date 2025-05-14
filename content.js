df215394c5ea6d2d89b182bdc027e2364f7c3791
dev-techics <info@techics.com>
2025-05-14 13:44:01 +0600

    version 1.0.0

---
+++ content.js
@@ -0,0 +1,23 @@
+// content.js
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+  if (request.action === "getBuyerInfo") {
+    // get the buyer name from the page
+    const buyerNameElement = document.querySelector('.buyer_name');
+    const name = buyerNameElement ? buyerNameElement.textContent.trim() : null;
+
+    // get the buyer phone from the page
+    const buyerPhoneElement = document.querySelector('.buyer-telephone-display');
+    const phone = buyerPhoneElement ? buyerPhoneElement.textContent.trim() : null;
+
+    const buyerEmailElement = document.querySelector('.buyer-email-display');
+    const email = buyerEmailElement ? buyerEmailElement.textContent.trim() : null;
+
+    sendResponse({ 
+        buyerName: name, 
+        buyerPhone: phone,
+        buyerEmail: email
+    });
+  }
+  return true;
+});
